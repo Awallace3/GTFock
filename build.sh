a6fa72282c8aa4333fd52b2bc92e44b383462868
Austin Wallace <austinwallace196@gmail.com>
2023-11-16 17:24:44 -0500

    cmake

--- build.sh
+++ build.sh
@@ -2,11 +2,10 @@
 rm -rf build
 mkdir -p build
 cd build
+
 export CMAKE_PREFIX_PATH=$CONDA_PREFIX:$CMAKE_PREFIX_PATH
-#export CC=icx
-#export CXX=icpx
-#export MPICC=mpiicx
-#export MPICXX=mpiicpx
+export WORK_TOP=/theoryfs2/ds/amwalla3/gits
+
 export CC=icc
 export CXX=icpc
 export MPICC=mpiicc
@@ -14,6 +13,6 @@ export MPICXX=mpiicpc
 
 export AR=xiar rcs
 
-cmake -DCMAKE_BUILD_TYPE=Debug -DCOMPILE_COMMANDS=ON ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DWORK_TOP=${WORK_TOP} ..
 make
 cd ..
