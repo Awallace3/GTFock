7311ec93eae444da5ef352c8c5e4bd02abea672a
Austin Wallace <austinwallace196@gmail.com>
2023-11-16 11:04:07 -0500

    icc

--- build.sh
+++ build.sh
@@ -3,11 +3,16 @@ rm -rf build
 mkdir -p build
 cd build
 export CMAKE_PREFIX_PATH=$CONDA_PREFIX:$CMAKE_PREFIX_PATH
-export CC=icx
-export CXX=icpx
+#export CC=icx
+#export CXX=icpx
+#export MPICC=mpiicx
+#export MPICXX=mpiicpx
+export CC=icc
+export CXX=icpc
+export MPICC=mpiicc
+export MPICXX=mpiicpc
+
 export AR=xiar rcs
-export MPICC=mpiicx
-export MPICXX=mpiicpx
 
 cmake -DCMAKE_BUILD_TYPE=Debug -DCOMPILE_COMMANDS=ON ..
 make
